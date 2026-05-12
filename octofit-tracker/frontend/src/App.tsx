5c432ba7f3d73182474cc6fcecdb8b5a3bd10f07
Dime Mishkov <dime.mishkov@gmail.com>
2026-05-12 11:15:08 +0000

    Step 1-5

--- octofit-tracker/frontend/src/App.tsx
+++ octofit-tracker/frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-
-import Activities from './components/Activities';
 import Leaderboard from './components/Leaderboard';
+import Activities from './components/Activities';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
+import OctofitLogo from './octofitapp-small.svg';
 
 function App() {
   return (
